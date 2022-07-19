55f3f0a5a70058f11446905ac776c2ffe292fc09
Rafael Singer <rafaelsinger66@gmail.com>
2022-07-19 16:24:24 -0700

    form now adds to db and main page displays db contents

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import (Flask, render_template, 
+from flask import (redirect, render_template, 
                     url_for, request)
-
-app = Flask(__name__)
+from models import db, Pet, app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    pets = Pet.query.all()
+    return render_template('index.html', pets=pets)
 
 @app.route('/pet')
 def pet():
@@ -13,9 +13,19 @@ def pet():
 
 @app.route('/add-pet', methods=['GET', 'POST'])
 def add_pet():
-    print(request.form)
-    print(request.form['name'])
+    if request.form:
+        new_pet = Pet(name=request.form['name'], age=request.form['age'],
+                    breed=request.form['breed'], color=request.form['color'],
+                    size=request.form['size'], weight=request.form['weight'],
+                    url=request.form['url'], url_tag=request.form['alt'],
+                    pet_type=request.form['pet'], gender=request.form['gender'],
+                    spay=request.form['spay'], house_trained=request.form['housetrained'],
+                    description=request.form['description'])
+        db.session.add(new_pet)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('addpet.html')
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
