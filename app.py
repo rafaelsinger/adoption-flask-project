9ac98946f76815d91cbdd97077b4f275d1a4f503
Rafael Singer <rafaelsinger66@gmail.com>
2022-07-18 23:47:20 -0700

    added stylesheets, additional routes, and started on form

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import (Flask, render_template, 
+                    url_for, request)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello from Pet Adoption"
+    return render_template('index.html')
 
-@app.route('/<name>')
-def name(name):
-    return "Hello {}".format(name)
+@app.route('/pet')
+def pet():
+    return render_template('pet.html')
+
+@app.route('/add-pet', methods=['GET', 'POST'])
+def add_pet():
+    print(request.form)
+    print(request.form['name'])
+    return render_template('addpet.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
