b96fd3538f370a225b42784f29c70c9c90c1434a
Rafael Singer <rafaelsinger66@gmail.com>
2022-07-19 19:54:57 -0700

    added edit and delete options and 404 handling

--- app.py
+++ app.py
@@ -7,9 +7,37 @@ def index():
     pets = Pet.query.all()
     return render_template('index.html', pets=pets)
 
-@app.route('/pet')
-def pet():
-    return render_template('pet.html')
+@app.route('/pet/<id>')
+def pet(id):
+    pet = Pet.query.get_or_404(id)
+    return render_template('pet.html', pet=pet)
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit_pet(id):
+    pet = Pet.query.get_or_404(id)
+    if request.form:
+        pet.name = request.form['name']
+        pet.age = request.form['age']
+        pet.breed = request.form['breed']
+        pet.color = request.form['color']
+        pet.size = request.form['size']
+        pet.url = request.form['url']
+        pet.url_tag = request.form['alt']
+        pet.pet_type = request.form['pet']
+        pet.gender = request.form['gender']
+        pet.spay = request.form['spay']
+        pet.house_trained = request.form['housetrained']
+        pet.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('editpet.html', pet=pet)
+
+@app.route('/delete/<id>')
+def delete_pet(id):
+    pet = Pet.query.get_or_404(id)
+    db.session.delete(pet)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 @app.route('/add-pet', methods=['GET', 'POST'])
 def add_pet():
@@ -25,6 +53,10 @@ def add_pet():
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('addpet.html')
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', msg=error), 404
 
 if __name__ == "__main__":
     db.create_all()
